13798ac33faf752a02e82e24362a62ab7e91b039
zimingzpp <zimingzpp@qq.com>
2020-01-23 23:44:42 +0800

    youcompleteme

---
+++ YouCompleteMe/b.go
@@ -0,0 +1,17 @@
+//*************************************
+//
+//作者:zimingzpp
+//Github:https://guthub.com/zimingzpp
+//创建时间:2020-01-19 16:17:05
+//文件名:b.go
+//文件描述:
+//文件编码: utf-8
+//*************************************
+
+pakage main
+import "fmt"
+func main(){
+	fmt.Println("Hello World!")
+}
+
+
